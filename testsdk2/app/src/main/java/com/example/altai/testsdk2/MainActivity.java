a323ff9caa09705f6c87d20b00e8146cbf0097cb
Odin9393 <Altai_9393@mail.ru>
2015-09-30 19:18:06 +0600

    Create MainAvtivity and  layout

--- testsdk2/app/src/main/java/com/example/altai/testsdk2/MainActivity.java
+++ testsdk2/app/src/main/java/com/example/altai/testsdk2/MainActivity.java
@@ -2,14 +2,36 @@ package com.example.altai.testsdk2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by Алтай on 30.09.2015.
- */
-public class MainActivity extends Activity {
+import java.util.TooManyListenersException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
-}
+
+        private void initToolbar(){
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle(R.string.app_name);
+            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return false;
+                }
+            });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+    }
+
