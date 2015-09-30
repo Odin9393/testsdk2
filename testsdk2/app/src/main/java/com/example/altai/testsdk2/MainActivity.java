01b85ac79bc844e5dba09eb0aad2b29423a9f09b
Odin9393 <Altai_9393@mail.ru>
2015-09-30 12:42:52 +0600

    Create MainAvtivity and  layout

---
+++ testsdk2/app/src/main/java/com/example/altai/testsdk2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.altai.testsdk2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Алтай on 30.09.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
